5a63a4f1c32ffbddd3259a632aaa54791722f4ca
gavin_lu <gavin_lu@kingdee.com>
2017-09-18 20:12:02 +0800

    euraca-client

---
+++ service-eureka-client/src/main/java/com/kd/gavin/service/Application.java
@@ -0,0 +1,18 @@
+package com.kd.gavin.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by kd_gavin on 2017/9/18,20:05.
+ *
+ * @dicription
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
